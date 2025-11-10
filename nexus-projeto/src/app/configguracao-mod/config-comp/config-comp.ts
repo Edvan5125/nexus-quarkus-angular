3f6fbe042eae99d43ebe7a36dc889ccfd43b2dd5
DojanDev <272136@unifio.edu.br>
2025-11-10 13:02:42 +0000

    commit-10/11/2025

--- nexus-projeto/src/app/configguracao-mod/config-comp/config-comp.ts
+++ nexus-projeto/src/app/configguracao-mod/config-comp/config-comp.ts
@@ -7,14 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./config-comp.css']
 })
 export class ConfigComp {
-  // Aparência (apenas visual dentro da página)
-  tema: 'claro' | 'escuro' = 'escuro';
+
+  // Aparência 
+
+  tema: 'claro' | 'escuro' = 'escuro' ;
   corPrimaria = '#a855f7';
   corBarraLateral = '#111827';
   corConversa = '#0b0b0b';
   tamanhoFonte = 14; // px
 
-  // Personalidade (visual; não impacta o chat)
+  // Personalidade 
   personalidade: 'Amigável' | 'Profissional' | 'Divertido' | 'Neutro' = 'Amigável';
+  
   descricaoPersonalidade = '';
+
 }
